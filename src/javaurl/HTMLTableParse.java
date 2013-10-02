b71590a3ca2845d4045a74de50d71a96c11d2b19
Peter Aldaron <peteraldaron@gmail.com>
2013-10-01 22:30:32 -0400

    first commit

---
+++ src/javaurl/HTMLTableParse.java
@@ -0,0 +1,28 @@
+/*
+ * Parses HTML table elements
+ * Input: a string that contains <table>...</table>
+ * Output:a string without the tags but still (sort of) keeps table organization.
+ */
+package javaurl;
+
+public class HTMLTableParse {
+	private String data;
+	public HTMLTableParse(String data){
+		this.data=data;
+	}
+	public String getString(){
+		//It works! More readable than the previous method too.
+		//returns the table:
+		String localdata=this.data;
+		//process th & td:
+		localdata=localdata.replaceAll("<t[h,d](\\s?.)*?>", "");
+		localdata=localdata.replaceAll("</t[h,d]>", " ");
+		//process tr:
+		localdata=localdata.replaceAll("<tr(\\s?.)*?>", "");
+		localdata=localdata.replaceAll("</tr>","\n");
+		//process the rest:
+		localdata=localdata.replaceAll("<(\\s?.)+?>", "");
+		localdata=localdata.replaceAll("&#160;", " ");
+		return localdata;
+	}
+}
